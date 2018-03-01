70eef8f99c3a462e4fbc4d562d2b8d0aa2d2437d
fredsalm <salmsimplon@gmail.com>
2018-03-01 18:03:02 +0100

    mise en place css

--- template-HTML/app/app.js
+++ template-HTML/app/app.js
@@ -1,3 +1,25 @@
 (function main() {
+
+	// var spending = "";
+	// var income = "";
+	
+
+// $('.valid').click(function(){
+
+// 	// $('.rent').val(spending)
+
+// 	console.log(spending)
+
+// 	$('.spending-reslt').append('coucou')
+
+
+// });
+
+$("#valid").click(function(){
+	console.log("oui");
+   $('.spending-reslt').text("Ton nouveau titre");
+   return false;
+});
+	
 	
 })(); //fonction main
